ede4c828843abaeb05e4e16f934684a911903822
Adam <adamcj@udel.edu>
2020-04-14 18:04:19 -0400

    Updates

--- alpha/sw.js
+++ alpha/sw.js
@@ -1,4 +1,4 @@
-importScripts("/educational-phaser-game-template/devprecache-manifest.9bbb2f9225da99ec00cb98ee8d5d5428.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/educational-phaser-game-template/alphaprecache-manifest.147cd040c08418cfa9cdefb3e402ca47.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
