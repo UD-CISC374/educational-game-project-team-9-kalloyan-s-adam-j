f47c872446287b3cbd06f5c6c30b3bcdfe0ddca1
Kalloyan Stoyanov <kalloyan@udel.edu>
2020-04-17 15:56:17 -0400

    Updates

---
+++ alpha/precache-manifest.f758acf7bb8b704cdf8ce33fd8b9a6d4.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e4866c2e280e7aee5f918672c0f59f",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphaassets/beaver_game/101-deer.png"
+  },
+  {
+    "revision": "5f1cab9a1fc92e1939fe99405b156374",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphaassets/beaver_game/101-skunk.png"
+  },
+  {
+    "revision": "374649352c5695135a0e3c00edaf7cf8",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphaassets/beaver_game/101-squirrel.png"
+  },
+  {
+    "revision": "8234b7302b2873892c288fd84be5f983",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphaassets/beaver_game/BG.png"
+  },
+  {
+    "revision": "cc36a75a3458508fbccc43d5d0d58604",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphaassets/beaver_game/BG2.png"
+  },
+  {
+    "revision": "200c4f483e6726bc2eef94dcdd82e7e3",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphaassets/beaver_game/beaver-NESW.png"
+  },
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphaassets/citations.md"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphaassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphaassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphaassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphaassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphaassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "4978e5cde168918a0a621f834b078f15",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphaassets/images/menu_background.jpg"
+  },
+  {
+    "revision": "452fee816764902b546f6ec860297c03",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphaassets/images/playbutton.PNG"
+  },
+  {
+    "revision": "b3e3e3b847e356f584170670b2c996b3",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphaassets/images/playbuttonhover.png"
+  },
+  {
+    "revision": "469e562f9bd54a416ae547fa6e80a8f5",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphaassets/images/playbuttonpress.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphafavicon.ico"
+  },
+  {
+    "revision": "f65283335d9df63c0b48",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphagen.main.30bd2887e1d98934851d.bundle.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphagen.vendors.0805bb80903235d40eaf.bundle.js"
+  },
+  {
+    "revision": "e414315b72fb1c9619dedcd3b2877fec",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphaindex.html"
+  },
+  {
+    "revision": "7cdf2ec9b1b3fc1a65f56e4a0d984144",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/educational-game-project-team-9-kalloyan-s-adam-j/alphasw.js"
+  }
+]);
--- alpha/sw.js
+++ alpha/sw.js
@@ -1,4 +1,4 @@
-importScripts("/https://github.com/UD-CISC374/educational-game-project-team-9-kalloyan-s-adam-j/alphaprecache-manifest.bedd39f066b9cedab05b9b000f4b956a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/educational-game-project-team-9-kalloyan-s-adam-j/alphaprecache-manifest.f758acf7bb8b704cdf8ce33fd8b9a6d4.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
